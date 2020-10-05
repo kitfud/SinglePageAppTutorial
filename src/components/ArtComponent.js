c6fe623dbaa1b0083b75546962c1b685d6bd431a
Kit Fuderich <kitfuderich@gmail.com>
2020-10-05 23:23:45 +0000

    Art Component Styled with the help of React JSX.

--- src/components/ArtComponent.js
+++ src/components/ArtComponent.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {ART} from './shared/art'
+
+function RenderPaintings(){
+  const mypaintings = ART.map((info)=>
+    <tr>
+      <td>{info.name}</td>
+      <td><img alt="" width= "200px" heigh="200px"src={info.image}></img></td>
+    </tr>
+  )
+
+  return <React.Fragment> 
+    <table>
+    <tr>
+      <th>Title</th>
+      <th>Painting</th>
+    </tr>
+    {mypaintings}
+    </table> 
+  </React.Fragment>
+}
 
 function Art(){
+
   return (
- <div>Art Component</div>
+    <React.Fragment>
+    <br/>
+    <RenderPaintings/>
+    <br/>
+    <Link style= {margin} to='/home'> <em>Click for Home</em> </Link>
+    </React.Fragment>
   )
 }
 
-export default Art
+const margin = {
+  marginLeft:"200px"
+}
+
+export default Art  

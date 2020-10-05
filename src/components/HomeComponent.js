b884d257b5fd29b2e9ed91cac7bd1a258a1646f5
Kit Fuderich <kitfuderich@gmail.com>
2020-10-05 22:50:40 +0000

    Home Component Styled and index.css, multiple lines of code for styling added.

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Home(){
   return (
- <div>Home Component</div>
+    <React.Fragment>
+      <text className ="center">
+      This is my website to showcase my wonderful art!
+      </text><br/>
+
+      <img alt="" className ="center" width="auto" height="auto" src= "/images/1.jpg"/><br/>
+      
+      <Link className="center" to='/art'><b style={size}>See The Collection</b></Link>
+    </React.Fragment>
   )
 }
 
+const size = {
+  fontSize:"30px"
+}
 
 export default Home
---
+++ src/components/shared/art.js
@@ -0,0 +1,38 @@
+export const ART = 
+[
+  {
+    id:1,
+    name:"Bad Bannana",
+    image:'/images/1.jpg'
+  },
+  {
+    id:2,
+    name:"The Future is Before Us",
+    image:'/images/2.jpg'
+  },
+  {
+    id:3,
+    name:"Flower",
+    image:'/images/3.jpg'
+  },
+  {
+    id:4,
+    name:"Abrstract Orange",
+    image:'/images/4.jpg'
+  },
+  {
+    id:5,
+    name:"Even Triangles!",
+    image:'/images/5.jpg'
+  },
+  {
+    id:6,
+    name:"Rialto Beach",
+    image:'/images/6.jpg'
+  },
+  {
+    id:7,
+    name:"Lambs Running Through Rose Bushes",
+    image:'/images/7.jpg'
+  }
+]
